de6362aca7232b73bc00b3c039d2f51ebb127eea
Maya Gao <maya@stripe.com>
2017-11-27 18:22:14 -0800

    prototype

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+/*
+    ./webpack.config.js
+*/
+
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
+      { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ }
+    ]
+  }
+}
